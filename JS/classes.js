f8a6fec942b83ac07758928e7359bdd729429f32
Marcin Grygierek <mg@itdotfocus.com>
2020-12-21 11:13:26 +0000

    classes

---
+++ JS/classes.js
@@ -0,0 +1,32 @@
+class Duck {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        console.log(`I am a duck named ${this.name}`);
+    }
+}
+
+class RubberDuck extends Duck {
+    constructor(name, level = 'medium') {
+        super(name);
+        this.level = level;
+    }
+    debug() {
+        console.log(`${this.level} duck says - Your code is wrong on so many levels`);
+    }
+}
+
+const duck = new Duck('Franek');
+duck.speak();
+
+const duck2 = new Duck('Tomek');
+duck2.speak();
+
+const rubberDuck = new RubberDuck('Quacky');
+rubberDuck.speak();
+rubberDuck.debug();
+
+const rubberDuckSr = new RubberDuck('Quacky sr', 'senior');
+rubberDuckSr.speak();
+rubberDuckSr.debug();
