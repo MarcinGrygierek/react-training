b657925e25ea30a127cc6967afa9df8f98739603
Marcin Grygierek <mg@itdotfocus.com>
2020-12-21 15:04:06 +0000

    conditional rendering, updated components

--- app/src/Hello.tsx
+++ app/src/Hello.tsx
@@ -1,3 +1,8 @@
- const Hello = () => <h1>Hello world!</h1>;
+ const Hello = () => {
+     const userInput = window.prompt('Podaj liczbę');
+     const number = userInput ? parseInt(userInput) : 0;
+     if(isNaN(number)) return <h1>Musisz podać liczbę</h1>
+     return <h1>{2 * number}</h1>
+ }
 
 export default Hello;
