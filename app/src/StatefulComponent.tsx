458ef7f545276f3d78ba69e2397375282ff1ff5d
Marcin Grygierek <mg@itdotfocus.com>
2020-12-22 09:50:01 +0000

    added seocnd filed to state

--- app/src/StatefulComponent.tsx
+++ app/src/StatefulComponent.tsx
@@ -1,29 +1,31 @@
 import React, {Component, useState} from 'react';
 
-// interface Props {}
+interface Props {}
 
-// interface State {
-//     name: string;
-// }
-
-// class StatefulComponent extends Component<Props, State> {
-//     constructor(props: Props) {
-//         super(props);
-//         this.state = {
-//             name: 'Marcin'
-//         }
-//     }
-
-//     render() {
-//         const {name} = this.state;
-//         return <h1>{name}</h1>
-//     }
-// }
-
-const StatefulComponent = () => {
-    const [name, setName] = useState<string>('Marcin');
-    const [age, setAge] = useState<number>(27);
-    return <h1>{name} ({age})</h1>
+interface State {
+    name: string;
+    age: number;
 }
+
+class StatefulComponent extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            name: 'Marcin',
+            age: 190,
+        }
+    }
+
+    render() {
+        const {name, age} = this.state;
+        return <h1>{name} ({age})</h1>
+    }
+}
+
+// const StatefulComponent = () => {
+//     const [name, setName] = useState<string>('Marcin');
+//     const [age, setAge] = useState<number>(27);
+//     return <h1>{name} ({age})</h1>
+// }
 
 export default StatefulComponent;
