38bbf1e077729c46844b2a48e1da29787622c24d
Marcin Grygierek <mg@itdotfocus.com>
2020-12-23 13:51:21 +0000

    redux thunk

--- app/src/LikesRedux/actions/index.ts
+++ app/src/LikesRedux/actions/index.ts
@@ -1,7 +1,8 @@
+import { Dispatch } from "react";
 import { LikesActions } from "../action-types";
 
 export const addLike = () => {
-    return (dispatch: any) => {
+    return (dispatch: Dispatch<{type: LikesActions}>) => {
         // symulacja asynchroniczności - docelowo znajdują się tutaj async/await i/lub promisy
         setTimeout(() => {
             dispatch({ type: LikesActions.ADD_LIKE });
@@ -10,3 +11,8 @@ export const addLike = () => {
 }
 
 export const removeLike = () => ({ type: LikesActions.REMOVE_LIKE });
+
+// export const fetchData = () => async (dispatch: Dispatch<{ type: Posts}>) => {
+//     const posts = await ApiService.get('/posts');
+//     dispatch({ type: Posts.FETCH_ALL, payload: posts})
+// }
